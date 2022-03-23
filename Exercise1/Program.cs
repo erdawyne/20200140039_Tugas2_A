9f106e3743d08802ba6cf792e273a13e3ba082d6
erdawyne <erdawynee1@gmail.com>
2022-03-24 04:14:59 +0700

    menteliti setiap syntax

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -19,7 +19,7 @@ namespace Exercise1
 
                 SqlCommand cm = new SqlCommand("create table Mall (nama_ruko varchar(20) not null primary key, nama_pemilik varchar(30), id_pemilik string(20), alamat string(30), phone int))" +
 
-                ("create table Sewa(id_sewa(varchar(10) not null primary key, tgl_booking date, uang_muka money, biaya_sewa money, status varchar(15))" +
+                ("create table Sewa(id_sewa varchar(10) not null primary key, tgl_booking date, uang_muka money, biaya_sewa money, status varchar(15))" +
 
                 ("create table Operasi (masa_kontrak varchar(20) not null primary key, tgl_selesai date, id_sewa varchar(10))" +
 
@@ -56,10 +56,10 @@ namespace Exercise1
                     + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Fashion','Lisa','A3','Godean','0897364289')"
                     + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Cooking Mom','Yati','A4','Turi','0853672484')"
                     + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Boba Jam','Lala','A5','Gamping','0820439439')"
-                     
+
                     +
                 //Tabel Sewa
-                    ("insert into Sewa (id_sewa,tgl_booking,uang_muka,biaya_sewa money,status values('SE01','20-03-2021','Rp. 500.000','Rp. 1.500.000','Kontrak')" 
+                    ("insert into Sewa (id_sewa,tgl_booking,uang_muka,biaya_sewa money,status values('SE01','20-03-2021','Rp. 500.000','Rp. 1.500.000','Kontrak')"
                     + ("insert into Sewa (id_sewa, tgl_booking, uang_muka, biaya_sewa money, status values('SE02','3-12-2021','Rp. 750.000','Rp. 1.500.000','Kontrak')"
                     + ("insert into Sewa (id_sewa, tgl_booking, uang_muka, biaya_sewa money, status values('SE03','10-12-2021','Rp. 500.000','Rp. 1.500.000','Kontrak')"
                     + ("insert into Sewa (id_sewa, tgl_booking, uang_muka, biaya_sewa money, status values('SE04','2-1-2022','Rp. 800.000','Rp. 1.500.000','Kontrak')"
@@ -67,7 +67,7 @@ namespace Exercise1
 
                     +
                 //Tabel Operasi
-                    ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('1 Tahun','20-03-2022','SE01')" 
+                    ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('1 Tahun','20-03-2022','SE01')"
                     + ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('3 Bulan','3-3-2022','SE02')"
                     + ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('2 Tahun','10-12-2024','SE03')"
                     + ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('2 Tahun','2-1-2024','SE04')"
@@ -76,7 +76,7 @@ namespace Exercise1
 
                     +
                 //Tabel Pembayaran
-                ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ('AA1','20-03-2021','Rp. 500.000','Rp. 6.000.000','SE01')" 
+                ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ('AA1','20-03-2021','Rp. 500.000','Rp. 6.000.000','SE01')"
                 + ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ('AA2','3-12-2021','Rp. 750.000','Rp. 4.500.000','SE02')"
                 + ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ('AA3','10-12-2021','Rp. 500.000','Rp. 12.000.000','SE03')"
                 + ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ('AA4','2-1-2022','Rp. 800.000','Rp. 12.000.000','SE04')"
@@ -96,6 +96,7 @@ namespace Exercise1
                 con.Close();
             }
         }
-        }
+        
     }
 }
+
