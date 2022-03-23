65c23b6e3427c3ca418c7f6d2c59a923299724d3
erdawyne <erdawynee1@gmail.com>
2022-03-24 03:43:27 +0700

    Mengupdate tambahan isi tabel mall

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -51,12 +51,24 @@ namespace Exercise1
                 con = new SqlConnection("data source=LAPTOP-EED9RP81\\ERDAWYNE;database=ERDA;Integrated Security = TRUE");
                 con.Open();
 
-                SqlCommand cm = new SqlCommand("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Hijabin','Tanjung','A1','Seyegan','0877222333')" 
-                    + ("insert into Sewa (id_sewa,tgl_booking,uang_muka,biaya_sewa money,status values('SE01','20-03-2021','Rp. 500.000','Rp. 1.500.000','Kontrak')" + 
+                SqlCommand cm = new SqlCommand("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Hijabin','Tanjung','A1','Seyegan','0877222333')"
+                    + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Maskerin','Bowo','A2','Minggir','0813232890')"
+                    + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Fashion','Lisa','A3','Godean','0897364289')"
+                    + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Cooking Mom','Yati','A4','Turi','0853672484')"
+                    + ("insert into Mall (nama_ruko,nama_pemilik,id_pemilik_alamat,phone values('Boba Jam','Lala','A5','Gamping','0820439439')", con);
 
-                    ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('Fashion','Lisa','A3','Godean','0897364289')" + 
+
+
+
+
+
+
+
+                    ("insert into Sewa (id_sewa,tgl_booking,uang_muka,biaya_sewa money,status values('SE01','20-03-2021','Rp. 500.000','Rp. 1.500.000','Kontrak')" + 
+
+                    ("Insert into Operasi (masa_kontrak,tgl_selesai,id_sewa values ('1 Tahun','20-03-2022','SE01')" + 
                     
-                    ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ("
+                    ("Insert into Pembayaran (id_pembayaran,tgl_bayar,jml_bayar,total_sewa,id_sewa values ('AA1','20-03-2021','Rp. 500.000','Rp. 6.000.000','SE01')" +
                     con);
             }
         }
