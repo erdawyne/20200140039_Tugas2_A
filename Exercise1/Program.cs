6259a742e60b47f829832b94702f0f34b612292f
erdawyne <erdawynee1@gmail.com>
2022-03-23 22:13:34 +0700

    membuat tabel mall

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Exercise1
 {
     class Program
     {
+        public void CreateTable()
+        {
+            SqlConnection con = null; 
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-EED9RP81\\ERDAWYNE;database=ERDA;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mall (nama_ruko varchar(20) not null primary key," +
+                    "Nama_pemilik varchar(30), id_pemilik string(20), alamat string(30), phone int))", con);
+                cm.ExecuteNonQuery();
+
+            }
+        }
         static void Main(string[] args)
         {
         }
