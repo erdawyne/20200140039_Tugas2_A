2b20a64b0de1423a9d894dda249fd45104d07d24
erdawyne <erdawynee1@gmail.com>
2022-03-24 04:15:54 +0700

    Mengganti tipe data

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -17,7 +17,7 @@ namespace Exercise1
                 con = new SqlConnection("data source=LAPTOP-EED9RP81\\ERDAWYNE;database=ERDA;Integrated Security = TRUE");
                 con.Open();
 
-                SqlCommand cm = new SqlCommand("create table Mall (nama_ruko varchar(20) not null primary key, nama_pemilik varchar(30), id_pemilik string(20), alamat string(30), phone int))" +
+                SqlCommand cm = new SqlCommand("create table Mall (nama_ruko varchar(20) not null primary key, nama_pemilik varchar(30), id_pemilik varchar(30), alamat varchar(20), phone int))" +
 
                 ("create table Sewa(id_sewa varchar(10) not null primary key, tgl_booking date, uang_muka money, biaya_sewa money, status varchar(15))" +
 
